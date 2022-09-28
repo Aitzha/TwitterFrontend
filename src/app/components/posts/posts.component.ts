7dcf6f644d1dfde90c96dae40095b4e387bbdee2
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2022-09-28 19:04:07 +0600

    Added create post feature

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -14,6 +14,8 @@ import {observable, Observable} from "rxjs";
 })
 export class PostsComponent implements OnInit {
 
+  fileToUpload: File | null = null;
+
   tempPosts!:PostModel[];
   posts:PostModel[] = [];
 
@@ -96,7 +98,7 @@ export class PostsComponent implements OnInit {
     this.posts = this.posts.filter((v, i) => i !== id);
   }
 
-  addPost(inputPost:HTMLInputElement) {
+  addPost(inputText:HTMLInputElement, inputUserId:HTMLInputElement, inputFile:HTMLInputElement) {
     // let img1: Blob = new Blob();
     // this.getFile().subscribe(data => {
     //   img1 = new Blob([data], {type: 'image/jpeg'});
@@ -105,45 +107,42 @@ export class PostsComponent implements OnInit {
     // reader.readAsDataURL(img1);
 
     let newPost: PostModel = {
-      ownerId: "localhost",
-      content: inputPost.value,
+      ownerId: inputUserId.value,
+      content: inputText.value,
       likesCount: 0,
       postedDate: new Date(),
-      imageURL: "place holder"}
+      imageURL: "place holder"};
 
-    this.posts.push(newPost);
+    if(inputFile.files != null) {
+      this.fileToUpload = inputFile.files.item(0);
+    }
+
+    this.postPosts(inputText.value, inputUserId.value);
   }
 
   getPosts(): Observable<any>{
-    return this.httpClient.get("https://sleepy-crag-50454.herokuapp.com/post", {responseType: "json", observe: "body"});
+    // return this.httpClient.get("https://sleepy-crag-50454.herokuapp.com/post", {responseType: "json", observe: "body"});
+    return this.httpClient.get("http://localhost:8080/post", {responseType: "json", observe: "body"});
   }
 
-  getHello() {
+  postPosts(text:string, userId:string){
+    let testData:FormData = new FormData();
+    if(this.fileToUpload != null) {
+      testData.append('image', this.fileToUpload);
+    }
 
-    // return this.http.get('http://localhost:8080/hello',{}).subscribe(data => {
-    //   console.log("Here will be return response code Ex :200", data)
-    //   this.message = data;
-    // });
+    this.httpClient.post("http://localhost:8080/post" + "?text=" + text + "&fakeOwnerId=" + userId,
+                                testData);
 
-    // return this.http.get('http://localhost:8080/hello',
-    //   {headers: new HttpHeaders('Access-Control-Allow-Origin'), responseType: "text", observe: "response"})
-    //   .subscribe(data => {this.message = data.statusText;});
+    this.posts = [];
 
+    this.getPosts().subscribe(data => {
+      for(let i = 0; i < data.length; i++) {
+        let newPost: PostModel = {ownerId: "temp", content: "something", likesCount: 0, postedDate: new Date, imageURL: "no image"};
+        console.log(data[i]);
+        Object.assign(newPost, data[i]);
+        this.posts.push(newPost);
+      }
+    })
   }
-  //
-  // getFile() {
-  //   return this.http.get('http://localhost:8080/test/image',
-  //     {headers: new HttpHeaders('Access-Control-Allow-Origin'), responseType: "blob", observe: "response"})
-  //     .subscribe(data => {this.posts[0].image = data.body;});
-  // }
-
-  // public getFile(): Observable<any>{
-  //   //   return this.http.get('https://twitter-bucket-2708.s3.eu-central-1.amazonaws.com/test.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220904T111051Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=AKIAYESCOPVACYRDXEFP%2F20220904%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Signature=6b47b34d8283bcb393eb60e69a9c51ce54323fa32e270261c510539035f28495', {responseType: "blob"});
-  //   // return this.httpClient.get("https://twitter-bucket-2708.s3.eu-central-1.amazonaws.com/test.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220904T122344Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=AKIAYESCOPVACYRDXEFP%2F20220904%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Signature=4293ee522915290cd211eef17de74ae870116a23e47c7a029eeffc2a3c4de1f5", {responseType: "blob"});
-  //   return this.httpClient.get("https://twitter-bucket-2708.s3.eu-central-1.amazonaws.com/test.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220905T061001Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=AKIAYESCOPVACYRDXEFP%2F20220905%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Signature=3005ed0e6f9c4f10a3e244563fd6a3ac3f8b8249b066031ea2248908bdb133da", {headers: new HttpHeaders()});
-  // }
-
-  // public getImage(): Observable<any> {
-  //   return this.http.get('http://localhost:8080/test/image', {responseType: "blob"});
-  // }
 }
